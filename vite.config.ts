2d5d836b4cf7d0120d6fc5d6c9693eb86a6ca1ad
ViniciusAugustoo <137912686+ViniciusAugustoo@users.noreply.github.com>
2025-07-11 11:34:53 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,11 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
-=======
-/*import { defineConfig } from "vite";
->>>>>>> importar-secundario
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-<<<<<<< HEAD
-}));
-=======
-}));*/
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-export default defineConfig(({ mode }) => ({
-  base: '/', // Adicione essa linha para definir o base do app
+  base: '/', // Adicionada do repositório secundário
   server: {
     host: "::",
     port: 8080,
@@ -50,4 +21,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
->>>>>>> importar-secundario
+
